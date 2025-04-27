b972a4bf2f9582f199fb4b65508d272e5a961a1b
Hysware <56950515+Hysware@users.noreply.github.com>
2025-04-27 14:43:06 +0800

    change imaginary part of optical potential

--- CC-SRC/111.sh
+++ CC-SRC/111.sh
@@ -1,64 +1,46 @@
-# 该程序在运行时，需添加一个参数，代表beta2,目的：分为多个后台运行，减小运行时间
+# just change potenteil parameters
 timeStart=$(date +%s)
 
 gfortran -o ./CC-SRC/cc/ccfull ./CC-SRC/cc/ccfull-sc.f  #compile the sorce file
 
-
-
-for((beta2=$1;beta2<$1+5;beta2+=10))   #beta2*1000,可以将beta2改为一个可以从其他地方读取的参数
+for((Vw=$1;Vw<$1+5;Vw+=10))   # vw * 10
 do
-	for((beta3=$2;beta3<$2+5;beta3+=10))   #beta3*1000
-	do
-		for((beta4=-60;beta4<90;beta4+=20))   # beta4 *1000
-		do
-		  for((V0 = 700; V0 < 780; V0 += 5))  ## add 2025.4.17 to change barrier height.
-		  ## for V0 in $(seq 69 0.5 72.5)  ## add 2025.4.17 to change barrier height.
-		  do
-			# build files
-			num1=$(printf "%03d" $beta2)
-			num2=$(printf "%03d" $beta3)
-			num3=$(printf "%03d" $beta4)
-      num4=$(printf "%03d" $V0   )
-			dirname=$num1$num2$num3$num4
-			mkdir $dirname
-			# copy files
-			cp ./CC-SRC/cc/ccfull-sc.inp ./$dirname/
-			cp ./CC-SRC/cc/ccfull ./$dirname/
-			cd $dirname
-			# changes 
-                        ###### here may be need changes
-			b2=$(echo "scale=3;$beta2/1000"|bc)
-			b3=$(echo "scale=3;$beta3/1000"|bc)
-			b4=$(echo "scale=3;$beta4/1000"|bc)
-      v0=$(echo "scale=3;$V0/10"     |bc)
+  for((Rw=$2;Rw<$2+5;Rw+=10))   # Rw *100
+  do
+    for((Aw=10;Aw<200;Aw+=20))   # Aw *1000
+    do
+      # build files
+      num1=$(printf "%03d" $Vw)
+      num2=$(printf "%03d" $Rw)
+      num3=$(printf "%03d" $Aw)
+      dirname=$num1$num2$num3
+      mkdir $dirname
+      # copy files
+      cp ./CC-SRC/cc/ccfull-sc.inp ./$dirname/
+      cp ./CC-SRC/cc/ccfull ./$dirname/
+      cd $dirname
+      # changes 
+      ###### here may be need changes
+      vw=$(echo "scale=3;$Vw/10  "|bc)
+      rw=$(echo "scale=3;$Rw/100 "|bc)
+      aw=$(echo "scale=3;$Aw/1000"|bc)
 
 
 
-                        ## also need to be changed with diff reactions. 
-			sed -e 3a\0.089,$b2,$b4,5 ./ccfull-sc.inp > cc.inp.tmp
-			mv ./cc.inp.tmp ./ccfull-sc.inp
-			sed "3d" ./ccfull-sc.inp > cc.inp.tmp
-			tr -d '\r' < ./cc.inp.tmp > ./ccfull-sc.inp
+      ## change potenteil parameters, row 8 in input file.
+      sed -e 8a\ $vw,$rw,$aw ./ccfull-sc.inp > cc.inp.tmp
+      mv ./cc.inp.tmp ./ccfull-sc.inp
+      sed "8d" ./ccfull-sc.inp > cc.inp.tmp
+      tr -d '\r' < ./cc.inp.tmp > ./ccfull-sc.inp
 
-			sed -e 4a\1.248,$b3,3,1 ./ccfull-sc.inp > cc.inp.tmp
-			mv ./cc.inp.tmp ./ccfull-sc.inp
-			sed "4d" ./ccfull-sc.inp > cc.inp.tmp
-			tr -d '\r' < ./cc.inp.tmp > ./ccfull-sc.inp
+      rm ./cc.inp.tmp
 
-			sed -e "7a\\$v0,1.2,0.65" ./ccfull-sc.inp > cc.inp.tmp    ### change V0
-			mv ./cc.inp.tmp ./ccfull-sc.inp
-			sed "7d" ./ccfull-sc.inp > cc.inp.tmp
-			tr -d '\r' < ./cc.inp.tmp > ./ccfull-sc.inp
-			  
-			rm ./cc.inp.tmp
-			
-			./ccfull < ccfull-sc.inp | tee -a output.txt
-			
-			# result collect
+      ./ccfull < ccfull-sc.inp | tee -a output.txt
 
-			cd ..
+      # result collect
+
+      cd ..
 # rm -rf $dirname
-done
 done
 done
 done
@@ -66,4 +48,3 @@ done
 timeEnd=$(date +%s)
 time=$((timeEnd-timeStart))
 echo "程序运行了 $time s"
-
